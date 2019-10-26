1f3458841235ddb319c289ce050ef54de0fd406e
aaditya.jindal <aaditya.jindal@WKSBAN24KAN0005.corp.mphasis.com>
2019-10-26 09:57:48 +0530

    Commit 3

--- MyFirstJavaGitHub/src/com/abc/MainClass.java
+++ MyFirstJavaGitHub/src/com/abc/MainClass.java
@@ -4,6 +4,9 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Account ac = new Account(1, "Aaditya");
+		System.out.println(ac);
 
 	}
 
